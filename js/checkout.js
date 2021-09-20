7a884c4817baf4fa64b20cec655ff49d106ea68d
Ezequiel Martinez <formacio@PC-1062599.formacio.local>
2021-09-20 13:12:58 +0200

    CHNGD: update Modal with products added to cart

--- js/checkout.js
+++ js/checkout.js
@@ -1,38 +1,163 @@
-// Get the input fields
-var password = document.querySelector(".password");
-var phone = document.querySelector(".phone");
-var firstName = document.querySelector(".first-name");
-var lastName = document.querySelector(".last-name");
-
-// Get the error elements
-var errorPassword = document.getElementById("errorPassword");
-var errorName = document.getElementById("errorName");
-var errorPhone = document.getElementById("errorPhone");
-
 // Exercise 9
+const form = document.getElementById("form");
 
-let allInputs = document.querySelectorAll(".input");
-var errorEmail = document.getElementById("errorEmail");
-var errorAddress = document.getElementById("errorAddress");
-var errorLastName = document.getElementById("errorLastName");
-var errorsGeneral = document.querySelectorAll(".error");
+const allInputs = document.getElementsByClassName("input");
+const firstName = document.getElementById("first-name");
+const email = document.getElementById("email");
+const address = document.getElementById("address");
+const lastName = document.getElementById("last-name");
+const password = document.getElementById("password");
+const phoneNumber = document.getElementById("phone-number");
+
+const displayMessage = (input, message) => {
+  const smallPrint = input.parentElement.querySelector("p");
+  smallPrint.classList.replace("hidden", "visible");
+  smallPrint.style.color = "red";
+  smallPrint.innerText = message;
+};
+
+const hideMessage = (input) => {
+  const smallPrint = input.parentElement.querySelector("p");
+  smallPrint.classList.replace("visible", "hidden");
+};
+
+const redBorder = (input) => {
+  if (input.classList.contains("valid")) {
+    input.classList.remove("valid");
+  }
+  input.classList.add("invalid");
+};
+
+const greenBorder = (input) => {
+  if (input.classList.contains("invalid")) {
+    input.classList.remove("invalid");
+  }
+  input.classList.add("valid");
+};
+
+const showError = (input, message) => {
+  displayMessage(input, message);
+  redBorder(input);
+};
+
+const showSuccess = (input) => {
+  hideMessage(input);
+  greenBorder(input);
+};
+
+const containsOtherThanNumbers = (input) => {
+  const re = /^[0-9]+$/;
+  const wrongPhoneFormat = !re.test(input.value);
+  if (wrongPhoneFormat) {
+    return true;
+  } else return false;
+};
+
+const containsOtherThanNumbersAndLetters = (input) => {
+  const re = /^[a-z0-9]+$/;
+  const wrongPassword = !re.test(input.value);
+  if (wrongPassword) {
+    return true;
+  } else return false;
+};
+
+const emailWrongFormat = (input) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const nonValid = !re.test(email.value);
+  if (nonValid) {
+    return true;
+  } else return false;
+};
+
+const containsOtherThanLetters = (input) => {
+  const re = /^[a-zA-Z]+$/;
+  const OnlyLetters = re.test(input.value);
+  if (!OnlyLetters) {
+    return true;
+  } else return false;
+};
+
+const tooShort = (input) => {
+  const tooShort = input.value.length < 3;
+  if (tooShort) {
+    return true;
+  } else return false;
+};
+
+const emptyField = (input) => {
+  if (input.value === "") {
+    return true;
+  } else return false;
+};
+
+const phoneNumberValid = () => {
+  if (emptyField(phoneNumber)) {
+    showError(phoneNumber, "Phone Number is required");
+  } else if (tooShort(phoneNumber)) {
+    showError(phoneNumber, "Phone number need to be at least 3 numbers");
+  } else if (containsOtherThanNumbers(phoneNumber)) {
+    showError(phoneNumber, "Must contain only numbers");
+  } else showSuccess(phoneNumber);
+};
+
+const passwordValid = () => {
+  if (emptyField(password)) {
+    showError(password, "Password is required");
+  } else if (tooShort(password)) {
+    showError(password, "Password need to be at least 3 characters");
+  } else if (containsOtherThanNumbersAndLetters(password)) {
+    showError(password, "Password can only contain numbers and letters");
+  } else showSuccess(password);
+};
+
+const emailValid = () => {
+  if (emptyField(email)) {
+    showError(email, "Email is required");
+  } else if (tooShort(email)) {
+    showError(email, "Must contain at least 3 characters");
+  } else if (emailWrongFormat(email)) {
+    showError(email, "Email is in wrong format");
+  } else showSuccess(email);
+};
+
+const addressValid = () => {
+  if (emptyField(address)) {
+    showError(address, "Address is required");
+  } else if (tooShort(address)) {
+    showError(address, "Must contain at least 3 characters");
+  } else showSuccess(address);
+};
+
+const lastNameValid = () => {
+  if (emptyField(lastName)) {
+    showError(lastName, "Last Name is required");
+  } else if (tooShort(lastName)) {
+    showError(lastName, "Must contain at least 3 letters");
+  } else if (containsOtherThanLetters(firstName)) {
+    showError(lastName, "Can only be letters");
+  } else showSuccess(lastName);
+};
+
+const firstNameValid = () => {
+  if (emptyField(firstName)) {
+    showError(firstName, "First Name is required");
+  } else if (tooShort(firstName)) {
+    showError(firstName, "Must contain at least 3 letters");
+  } else if (containsOtherThanLetters(firstName)) {
+    showError(firstName, "Can only be letters");
+  } else showSuccess(firstName);
+};
 
 function validate() {
-  // Validate fields entered by the user: name, phone, password, and email
-
-  console.log("submitted")
-  /* for (let i = 0; i < allInputs.length; i++) {
-    if (allInputs[i].value.trim().length < 3) {
-      allInputs[i].classList.add("border-danger");
-      errorsGeneral[i].innerHTML =
-        "All fields need to have at least 3 characters";
-      errorsGeneral[i].style.display = "block";
-    }
-
-    if (allInputs[i].value === "" || allInputs[i].value === null) {
-      allInputs[i].classList.add("border-danger");
-      errorsGeneral[i].innerHTML = "All fields are mandatory";
-      errorsGeneral[i].style.display = "block";
-    }
-  } */
+  firstNameValid();
+  lastNameValid();
+  emailValid();
+  passwordValid();
+  addressValid();
+  phoneNumberValid();
 }
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
--- js/grocery.js
+++ js/grocery.js
@@ -199,9 +199,12 @@ function applyPromotionsCart() {
   // Apply promotions to each item in the array "cart"
   for (let i = 0; i < cart.length; i++) {
     cart[i].subtotal = cart[i].quantity * cart[i].price;
-    if (cart[i].name === "cooking oil" && cart[i].quantity >=3) {
+    if (cart[i].name === "cooking oil" && cart[i].quantity >= 3) {
       cart[i].subtotalWithDiscount = cart[i].quantity * (cart[i].price - 0.5);
-    } else if (cart[i].name === "Instant cupcake mixture" && cart[i].quantity >=10) {
+    } else if (
+      cart[i].name === "Instant cupcake mixture" &&
+      cart[i].quantity >= 10
+    ) {
       cart[i].subtotalWithDiscount = Math.round(
         (cart[i].quantity * cart[i].price * 2) / 3
       );
@@ -213,120 +216,129 @@ function applyPromotionsCart() {
 // Exercise 8
 function addToCart(id) {
   // 1. Loop for to the array products to get the item to add to cart
-  let cartItem = {}
+  let cartItem = {};
   let selectedItem;
   let index;
 
-  for (let i=0; i<products.length; i++){
-    if (i +1 === id) {
+  for (let i = 0; i < products.length; i++) {
+    if (i + 1 === id) {
       selectedItem = products[i];
     }
   }
 
-  if(cart.some(item => selectedItem.name === item.name)){
+  if (cart.some((item) => selectedItem.name === item.name)) {
     index = cart.findIndex((item) => item.name === selectedItem.name);
     cart[index].quantity = cart[index].quantity + 1;
-    cart[index].subtotal = cart[index].price *cart[index].quantity; 
-    
-    if(cart[index].name === 'cooking oil'){
-      if(cart[index].quantity >= 3){
-        cart[index].subtotalWithDiscount = cart[index].quantity * (cart[index].price - 0.5);
-      } 
-    } else if (cart[index].name === 'Instant cupcake mixture'){
-      if(cart[index].quantity >= 10){
+    cart[index].subtotal = cart[index].price * cart[index].quantity;
+
+    if (cart[index].name === "cooking oil") {
+      if (cart[index].quantity >= 3) {
+        cart[index].subtotalWithDiscount =
+          cart[index].quantity * (cart[index].price - 0.5);
+      }
+    } else if (cart[index].name === "Instant cupcake mixture") {
+      if (cart[index].quantity >= 10) {
         cart[index].subtotalWithDiscount = Math.round(
           (cart[index].quantity * cart[index].price * 2) / 3
         );
       }
-    } 
+    }
   } else {
     cartItem = {
       ...selectedItem,
       quantity: 1,
       subtotal: selectedItem.price,
-    }
-    cart.push(cartItem)
+    };
+    cart.push(cartItem);
   }
-  
 }
 
 // Exercise 9
 
-const updateSubtotalWithDiscount = (selectedItem, isCookingOil, isInstantCupcakeMixture) => {
-  if (isCookingOil)selectedItem.subtotalWithDiscount =
-  selectedItem.quantity * (selectedItem.price - 0.5);
+const deleteSubtotalWithDiscountProperty = (selectedItem) =>
+  delete selectedItem.subtotalWithDiscount;
 
-  if (isInstantCupcakeMixture)selectedItem.subtotalWithDiscount = Math.round(
-    (selectedItem.quantity * selectedItem.price * 2) / 3
-  )
-  
-}
+const updateSubtotalWithDiscount = (
+  selectedItem,
+  isCookingOil = false,
+  isInstantCupcakeMixture = false
+) => {
+  if (isCookingOil) {
+    selectedItem.subtotalWithDiscount =
+      selectedItem.quantity * (selectedItem.price - 0.5);
+  }
 
-const deleteSubtotalWithDiscount = selectedItem => delete selectedItem.subtotalWithDiscount
-  
-  const checkIfDiscountsApplyAndUpdate = selectedItem => {
-    const isCookingOil = selectedItem.name === "cooking oil"
-    const isInstantCupcakeMixture = selectedItem.name === "Instant cupcake mixture"
-    const LessThan3Units = selectedItem.quantity < 3
-    const LessThan10Units = selectedItem.quantity < 10
-  if (isCookingOil) 
-      {
-        if (LessThan3Units) deleteSubtotalWithDiscount(selectedItem)
-        else updateSubtotalWithDiscount(selectedItem, isCookingOil)
-      } 
-    else 
-      {
-  if (isInstantCupcakeMixture) 
-    {
-        if (LessThan10Units) deleteSubtotalWithDiscount(selectedItem)
-        else updateSubtotalWithDiscount(selectedItem, isInstantCupcakeMixture)
-        }
+  if (isInstantCupcakeMixture) {
+    selectedItem.subtotalWithDiscount = Math.round(
+      (selectedItem.quantity * selectedItem.price * 2) / 3
+    );
+  }
+};
+
+const checkIfDiscountsApplyAndUpdate = (selectedItem) => {
+  const isCookingOil = selectedItem.name === "cooking oil";
+  const isInstantCupcakeMixture =
+    selectedItem.name === "Instant cupcake mixture";
+  const LessThan3Units = selectedItem.quantity < 3;
+  const LessThan10Units = selectedItem.quantity < 10;
+  if (isCookingOil) {
+    if (LessThan3Units) {
+      deleteSubtotalWithDiscountProperty(selectedItem);
+    } else {
+      updateSubtotalWithDiscount(selectedItem, isCookingOil);
+    }
+  } else {
+    if (isInstantCupcakeMixture) {
+      if (LessThan10Units) {
+        deleteSubtotalWithDiscountProperty(selectedItem);
+      } else {
+        updateSubtotalWithDiscount(selectedItem, isInstantCupcakeMixture);
       }
     }
+  }
+};
 
-
-const updateQuantityAndSubtotal= (selectedItem) =>{
-  selectedItem.quantity = selectedItem.quantity -1 ;
+const updateQuantityAndSubtotal = (selectedItem) => {
+  selectedItem.quantity = selectedItem.quantity - 1;
   selectedItem.subtotal = selectedItem.quantity * selectedItem.price;
-}
+};
 
+const findPositionInCartItemToRemove = (selectedItem) =>
+  cart.findIndex((item) => item.name === selectedItem.name);
 
-const findPositionInCartItemToRemove = selectedItem =>  cart.findIndex((item) => item.name === selectedItem.name);
- 
-const removeSelectedItemfromCart = position => {
+const removeSelectedItemfromCart = (position) => {
   cart.splice(position, 1);
-}
+};
 
-const isSelectedItemInCart = selectedItem => cart.some((item) => selectedItem.name === item.name)
+const isSelectedItemInCart = (selectedItem) =>
+  cart.some((item) => selectedItem.name === item.name);
 
-const findSelectedItem = id => {
+const findSelectedItem = (id) => {
   let selectedItem;
-  for (let i=0; i<cart.length; i++){
-    if (i+1 === id) {
+  for (let i = 0; i < cart.length; i++) {
+    if (i + 1 === id) {
       selectedItem = cart[i];
     }
   }
   return selectedItem;
-}
-
+};
 
 function removeFromCart(id) {
-  const selectedItem = findSelectedItem(id)
-  const moreThanOneSelectedItemInCart =  selectedItem.quantity > 1
+  const selectedItem = findSelectedItem(id);
+  const moreThanOneSelectedItemInCart = selectedItem.quantity > 1;
 
   if (isSelectedItemInCart(selectedItem)) {
-
     if (moreThanOneSelectedItemInCart) {
-      updateQuantityAndSubtotal(selectedItem)
-      checkIfDiscountsApplyAndUpdate(selectedItem)
-      
+      updateQuantityAndSubtotal(selectedItem);
+      checkIfDiscountsApplyAndUpdate(selectedItem);
     } else {
-      const position = findPositionInCartItemToRemove(selectedItem)
-      removeSelectedItemfromCart(position)
+      const position = findPositionInCartItemToRemove(selectedItem);
+      removeSelectedItemfromCart(position);
     }
   }
   console.log("cart>=", cart);
 }
+
 
 
 // Exercise 10
